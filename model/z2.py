fd518cd934146c0d8b86aa1fe03dbde567c3d42e
ML <99159231+ML202010@users.noreply.github.com>
2025-12-20 13:48:21 +0800

    Update z2.py

--- model/z2.py
+++ model/z2.py
@@ -4,8 +4,6 @@ import torch
 import torch.nn as nn
 import torch
 from torch import nn
-
-
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels=None):
         super(ConvBlock, self).__init__()
@@ -33,8 +31,6 @@ class ConvBlock(nn.Module):
         x = self.bn2(x)
         x = self.act(x)
         return x
-
-
 class ChannelAttention(nn.Module):
     def __init__(self, in_channels, reduction=16):
         super(ChannelAttention, self).__init__()
@@ -67,7 +63,6 @@ class SpatialAttention(nn.Module):
             nn.Conv2d(1, 1, kernel_size=1),
             nn.Sigmoid()
         )
-
     def forward(self, x):
         mean_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
@@ -96,7 +91,6 @@ class CASAB(nn.Module):
         x = self.spatial_attention(x)
 
         return x + identity
-
 class EdgeEnhancer(nn.Module):
     def __init__(self, in_dim, norm, act):
         super().__init__()
@@ -112,8 +106,6 @@ class EdgeEnhancer(nn.Module):
         edge = x - edge
         edge = self.out_conv(edge)
         return x + edge
-
-
 class MEEM(nn.Module):
     def __init__(self, in_dim, hidden_dim, width=4, norm=nn.BatchNorm2d, act=nn.ReLU):
         super().__init__()
@@ -126,12 +118,9 @@ class MEEM(nn.Module):
             norm(hidden_dim),
             nn.Sigmoid()
         )
-
         self.pool = nn.AvgPool2d(3, stride=1, padding=1)
-
         self.mid_conv = nn.ModuleList()
         self.edge_enhance = nn.ModuleList()
-
         for i in range(width - 1):
             self.mid_conv.append(nn.Sequential(
                 nn.Conv2d(hidden_dim, hidden_dim, 1, bias=False),
@@ -145,20 +134,15 @@ class MEEM(nn.Module):
             norm(in_dim),
             act()
         )
-
         self.ca =CASAB(in_channels=in_dim)
-
     def forward(self, x):
         mid = self.in_conv(x)
         out = mid
-
         for i in range(self.width - 1):
             mid = self.pool(mid)
             mid = self.mid_conv[i](mid)
             edge = self.edge_enhance[i](mid)
             out = torch.cat([out, edge], dim=1)
-
         out = self.out_conv(out)
-
         out = self.ca(out) * out 
         return out
